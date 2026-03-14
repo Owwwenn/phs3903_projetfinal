0ae11b96b9c61cba783b6d1616076751e12d10d4
Owwwenn <owensam042@gmail.com>
2026-03-14 17:36:31 -0400

    Finir la fonction inverse norme

--- simulation_core/potential/norms.py
+++ simulation_core/potential/norms.py
@@ -1,7 +1,7 @@
 import numpy as np
 import quaternion as qtn
 
-def build_norm_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:float, r_oh:float):
+def build_inv_norm_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:float, r_oh:float):
     """Construit les matrices de normes entre les différentes sites des molécules de l'eau.
 
     Args:
@@ -13,7 +13,7 @@ def build_norm_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:f
         nbr_list (_type_): Liste de booléens des voisins pour chaque molécule
 
     Returns:
-        [M_OO, M_OH1, M_OH2, M_H1O, M_H1H1, M_H1H2, M_H2O, M_H2H1, M_H2H2]: 9 matrices de taille nxn qui donnent les distances entre chaque site.
+        [M_OO, M_OH1, M_OH2, M_H1O, M_H1H1, M_H1H2, M_H2O, M_H2H1, M_H2H2]: 9 matrices de taille nxn qui donnent les inverses des distances entre chaque site.
     """ 
     M_OO = np.zeros((n,n))
     M_OH1 = np.zeros((n,n))
@@ -38,20 +38,32 @@ def build_norm_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:f
                 # Définition de r et de q relatifs
                 ri = list_r[j] - list_r[i]             
                 r = ri - np.dot(L, np.round(np.dot(ri, 1/L)))
-                q = qtn.quaternion(list_q[i]).conjugate() * qtn.quaternion(list_q[j]) 
+                q = qtn.from_float_array(list_q[i]).conjugate() * qtn.from_float_array(list_q[j]) 
 
-                # Définition des 
-                r_h1 =  
-    """
-    TODO:
-    - Ajouter le calcul de distances
-    - Ajouter condition frontières périodiques
-    """
+                # Définition des positions des sites hydrogènes dans le repère de la molécule
+                r_h1 = r_oh * np.array([0,s,c])  
+                r_h2 = r_oh * np.array([0,-s,c])  
+
+                # Définition des vecteurs dans le repère monde
+                u_h1 = qtn.rotate_vectors(q, r_h1) 
+                u_h2 = qtn.rotate_vectors(q, r_h2) 
+
+                # Calcul des inverses des distances
+                M_OO[i, j] = 1 / np.sqrt(r @ r)
+                M_OH1[i, j] = 1 / np.sqrt((r + u_h1) @ (r + u_h1))
+                M_OH2[i, j] = 1 / np.sqrt((r + u_h2) @ (r + u_h2))
+                M_H1O[i, j] = 1 / np.sqrt((r - r_h1) @ (r - r_h1))
+                M_H1H1[i, j] = 1 / np.sqrt((r - r_h1 + u_h1) @ (r - r_h1 + u_h1))
+                M_H1H2[i, j] = 1 / np.sqrt((r - r_h1 + u_h2) @ (r - r_h1 + u_h2))
+                M_H2O[i, j] = 1 / np.sqrt((r - r_h2) @ (r - r_h2))
+                M_H2H1[i, j] = 1 / np.sqrt((r - r_h2 + u_h1) @ (r - r_h2 + u_h1))
+                M_H2H2[i, j] = 1 / np.sqrt((r - r_h2 + u_h2) @ (r - r_h2 + u_h2))
     
     return [M_OO, M_OH1, M_OH2, M_H1O, M_H1H1, M_H1H2, M_H2O, M_H2H1, M_H2H2]
 
 """
 TODO
-- Ajouter les distances O-H1, O-H2, H1-O, H1-H1, H1-H2, H2-O, H2-H1, H2-H2, en fonction de la matrice déjà créée et les quaternions de chaque molécule?
-- Vérifier si l'idée est possible 
+- Rajouter des tests dans ifname main pour vérifier que la fonciton fonctionne
+- Enlever la double boucle et vectoriser avec numpy
+- Passer à Julia si je veux garder la boucle
 """
