4f2b3962ded77bc45abfab07fe1306f9da486b78
Owwwenn <owensam042@gmail.com>
2026-03-27 16:30:10 -0400

    aider src au path de time int vectorized py

--- src/simulation_core/time_integrator/time_int_VECTORISED.py
+++ src/simulation_core/time_integrator/time_int_VECTORISED.py
@@ -1,8 +1,10 @@
-
+import sys
+sys.path.append("./src")
 import numpy as np
 from scipy.spatial.transform import Rotation
-from src.simulation_core.potential_force.potentiel_force import build_potential_vector_force_torque_matrix
-from src.simulation_core.potential_force.norms import build_inv_norm_matrix
+from simulation_core.potential_force.potentiel_force import build_potential_vector_force_torque_matrix
+from simulation_core.potential_force.norms import build_inv_norm_matrix
+
 # =============================================================================
 # CONSTANTS
 # =============================================================================
