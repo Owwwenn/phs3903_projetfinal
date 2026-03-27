5b71e11ba1e2e0d00bc8cc32b44fc0988fdb6e62
Owwwenn <owensam042@gmail.com>
2026-03-27 14:02:40 -0400

    Add: fonciton potentiel + force

--- src/simulation_core/potential_force/potentiel_force.py
+++ src/simulation_core/potential_force/potentiel_force.py
@@ -1,4 +1,4 @@
-from simulation_core.potential.norms import build_inv_norm_matrix
+from simulation_core.potential_force.norms import build_inv_norm_matrix
 import numpy as np
 
 def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:float, r_oh:float, q_o:float, q_h:float):
@@ -17,14 +17,8 @@ def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz
         q_h (float): Charge sur l'atome d'hydrogène
 
     Returns:
-        numpy.array: Vecteur de taille n contenant le potentiel appliqué sur chaque molécule.
+        numpy.array: Vecteur de taille n contenant le potentiel appliqué sur chaque molécule. Et la force appliquée sur chaque molécule selon (x, y, z, tau_x, tau_y, tau_z) dans le repère de la molécule.
     """    
-    list_q = v[6*n:10*n].reshape(n,4)[:, [3,0,1,2]] 
-    w = list_q[:, 0]; a = list_q[:, 1]; b = list_q[:, 2]; c = list_q[:, 3]
-    G = 2*np.array([[-a, w, -c, b],
-                    [-b, c, w, -a],
-                    [-c, -b, a, w]])
-
     [inv_norm_matrix_list, deriv_matrices]= build_inv_norm_matrix(n, v, Lx, Ly, Lz, nbr_list, theta, r_oh)
     inv_norm_matrix_list = np.array(inv_norm_matrix_list)
     deriv_matrices = np.array(deriv_matrices)
@@ -34,10 +28,7 @@ def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz
     U = np.einsum("ki,kij,kj->i", left_charge_vector_list, inv_norm_matrix_list, right_charge_vector_list)
 
     inml2 = (inv_norm_matrix_list**2)[..., None]
-    grad = np.einsum("ij,ijkl,ik->jl", left_charge_vector_list, -deriv_matrices * inml2, right_charge_vector_list)
-    F_lin = -grad[:, :3]
-    tau = np.einsum("ijk,kj->ki", G, grad[:, 3:])
-    F = np.array([F_lin.reshape(n,3), tau.reshape(n,3)]).reshape(n,6)
+    F = np.einsum("ij,ijkl,ik->jl", left_charge_vector_list, deriv_matrices * inml2, right_charge_vector_list)
 
     return U, F
 
