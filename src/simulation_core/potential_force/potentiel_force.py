5b69a35dbb2484b0d3c0d30988342c170f518909
Owwwenn <owensam042@gmail.com>
2026-03-30 10:59:52 -0400

    Add: LJ potential dans potentiel_force.py

--- src/simulation_core/potential_force/potentiel_force.py
+++ src/simulation_core/potential_force/potentiel_force.py
@@ -1,7 +1,7 @@
 from simulation_core.potential_force.norms import build_inv_norm_matrix
 import numpy as np
 
-def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:float, r_oh:float, q_o:float, q_h:float):
+def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz:float, nbr_list, theta:float, r_oh:float, q_o:float, q_h:float, eps_LJ: float, sigma_LJ: float):
     """Construit le vecteur de potentiel et le vecteur de force et de torque appliqué sur chacune des n molécules du système. 
 
     Args:
@@ -15,6 +15,8 @@ def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz
         r_oh (float): Distance O-H du modèle de la molécule d'eau
         q_o (float): Charge sur l'atome d'oxygène
         q_h (float): Charge sur l'atome d'hydrogène
+        eps_LJ (float): epsilon de Lennard-Jones pour l'interaction O-O 
+        sigma_LJ (float): sigma de Lennard-Jones pour l'interaction O-O 
 
     Returns:
         numpy.array: Vecteur de taille n contenant le potentiel appliqué sur chaque molécule. Et la force appliquée sur chaque molécule selon (x, y, z, tau_x, tau_y, tau_z) dans le repère de la molécule.
@@ -25,10 +27,18 @@ def build_potential_vector_force_torque_matrix(n: int, v, Lx:float, Ly:float, Lz
     left_charge_vector_list = np.array([q_o*np.ones(n)]*3 + [q_h*np.ones(n)]*6)
     right_charge_vector_list = np.array(([q_o*np.ones(n)] + [q_h*np.ones(n)]*2) * 3)
 
-    U = np.einsum("ki,kij,kj->i", left_charge_vector_list, inv_norm_matrix_list, right_charge_vector_list)
-
+    # Potentiel et forces pour Coulomb
     inml2 = (inv_norm_matrix_list**2)[..., None]
-    F = np.einsum("ij,ijkl,ik->jl", left_charge_vector_list, deriv_matrices * inml2, right_charge_vector_list)
+    U_coulomb = np.einsum("ki,kij,kj->i", left_charge_vector_list, inv_norm_matrix_list, right_charge_vector_list)
+    F_coulomb = np.einsum("ij,ijkl,ik->jl", left_charge_vector_list, deriv_matrices * inml2, right_charge_vector_list)
+
+    # Potentiel et forces pour Lennard-Jones
+    inv_rOO = inv_norm_matrix_list[0]; grad_rOO = deriv_matrices[0]
+    U_LJ = np.sum(4*eps_LJ*((sigma_LJ*inv_rOO)**12 - (sigma_LJ*inv_rOO)**6), axis=0)
+    F_LJ = np.einsum('ij,ijk->ik', 4*eps_LJ*((6*sigma_LJ**6) * (inv_rOO**7) - (12*sigma_LJ**12) * (inv_rOO**13)), grad_rOO)
+
+    U = U_coulomb + U_LJ
+    F = F_coulomb + F_LJ 
 
     return U, F
 
