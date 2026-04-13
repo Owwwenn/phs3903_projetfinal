ab0c6fbbd88a9b0524899334b496e0ea495530eb
KJ <jahed.karim@outlook.com>
2026-04-13 16:17:07 -0400

    commit

--- scripts/main.py
+++ scripts/main.py
@@ -4,16 +4,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from src.md_sim.core.system import get_site_offsets 
-from src.md_sim.core.system import make_initial_state
-from src.md_sim.core.potential_force.coul_LJ import compute_forces_and_torques
-from src.md_sim.core.system import get_atom_positions
-from src.md_sim.core.neighbour_list.neighbour_list import build_neighbour_list
-from src.md_sim.core.system import SPCE
-from src.md_sim.core.system import T_UNIT_PS
-from src.md_sim.caracterisation.energy import kinetic_energy_rot
-from src.md_sim.caracterisation.energy import kinetic_energy_trans
-from src.md_sim.core.time_integrator.time_int_VECTORISED import velocity_verlet_step
+from phs3903_projetfinal.src.md_sim.core.system import get_site_offsets 
+from phs3903_projetfinal.src.md_sim.core.system import make_initial_state
+from phs3903_projetfinal.src.md_sim.core.potential_force.coul_LJ import compute_forces_and_torques
+from phs3903_projetfinal.src.md_sim.core.system import get_atom_positions
+from phs3903_projetfinal.src.md_sim.core.neighbour_list.neighbour_list import build_neighbour_list
+from phs3903_projetfinal.src.md_sim.core.system import SPCE
+from phs3903_projetfinal.src.md_sim.core.system import T_UNIT_PS
+from phs3903_projetfinal.src.md_sim.caracterisation.energy import kinetic_energy_rot
+from phs3903_projetfinal.src.md_sim.caracterisation.energy import kinetic_energy_trans
+from phs3903_projetfinal.src.md_sim.core.time_integrator.time_int_VECTORISED import velocity_verlet_step
 
 # ─────────────────────────────────────────────
 #  Simulation parameters
